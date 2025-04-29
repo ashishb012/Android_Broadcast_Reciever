304cac4bc82e7708ba528526c7cf3c560c9620cc
ashishb012 <ashishb0128@gmail.com>
2025-04-29 12:56:35 +0530

    Modified

--- app/src/main/java/com/example/androidbroadcastreciever/MainActivity.java
+++ app/src/main/java/com/example/androidbroadcastreciever/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.androidbroadcastreciever;
 
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -13,10 +12,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private static final String CUSTOM_INTENT_ACTION = "com.example.androidbroadcastreciever.MY_CUSTOM_INTENT";
-    private static final String APP2_PACKAGE_NAME = "com.example.androidlocalbroadcast"; // Replace with App 2's package name
-    private static final String APP2_RECEIVER_CLASS = "com.example.androidlocalbroadcast.GlobalBroadcastReceiver"; // Replace with App 2's receiver class name
     private NetworkChangeReceiver networkChangeReceiver;
-    private App2Receiver app2Receiver;
+    private CustomBroadcastReceiver customBroadcastReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,28 +27,22 @@ public class MainActivity extends AppCompatActivity {
                 sendCustomBroadcast();
             }
         });
-
         networkChangeReceiver = new NetworkChangeReceiver(this);
         networkChangeReceiver.register();
 
-        // Register receiver for app2 broadcast
-        app2Receiver = new App2Receiver();
-        IntentFilter intentFilter = new IntentFilter("com.example.androidlocalbroadcast.MY_GLOBAL_BROADCAST");
-        registerReceiver(app2Receiver, intentFilter);
+        customBroadcastReceiver = new CustomBroadcastReceiver();
+        registerReceiver(customBroadcastReceiver, new IntentFilter(CUSTOM_INTENT_ACTION), Context.RECEIVER_EXPORTED);
     }
+
 
     private void sendCustomBroadcast() {
         Intent customIntent = new Intent(CUSTOM_INTENT_ACTION);
-        // Explicitly set the component name to target App 2's receiver
-        ComponentName componentName = new ComponentName(APP2_PACKAGE_NAME, APP2_RECEIVER_CLASS);
-        customIntent.setComponent(componentName);
         sendBroadcast(customIntent);
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
         networkChangeReceiver.unregister();
-        unregisterReceiver(app2Receiver);
+        unregisterReceiver(customBroadcastReceiver);
     }
 }
